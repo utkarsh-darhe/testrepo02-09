bcedb86098ad334a29ff30328173de3b25319496
Utkarsh <utkarshdarhe95@gmail.com>
2022-09-02 12:29:50 +0000

    this is the first commit

---
+++ commands.sh
@@ -0,0 +1,18 @@
+ yum update -y
+    2  yum install httpd -y
+    3  ls -la
+    4  cd ..
+    5  ls -la
+    6  cd /var
+    7  ls -al
+    8  cd www
+    9  ls
+   10  cd html
+   11  ls -al
+   12  vi index.html
+   13  cat index.html
+   14  service httpd status
+   15  service httpd start
+   16  service httpd status
+# in vi index.html command you can put the data that needs to be hosted.
+
